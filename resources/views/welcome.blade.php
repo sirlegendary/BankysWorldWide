0bf5caabd253ee0c21245895f3ac272ee51a52b7
Wale Salami 2 <wale@wsalami.com>
2017-03-11 00:02:06 +0000

    still part of first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel1
+                    Laravel
                 </div>
 
                 <div class="links">
