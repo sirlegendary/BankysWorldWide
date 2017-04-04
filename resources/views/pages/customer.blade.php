8417cee8b0182835b615d5bb30e33d908cf4a331
Wale Salami 2 <wale@wsalami.com>
2017-04-04 14:18:11 +0100

    Search bar complete

--- resources/views/pages/customer.blade.php
+++ resources/views/pages/customer.blade.php
@@ -5,14 +5,9 @@
 
 	<div class="jumbotron">
 	  
-	  <h2>Search for customer</h2>
+	  <h2>{{ count($listCustomer) }} Customers - Search</h2>
 
-	  	<form role="search">
-		  <div class="form-group">
-		    <input type="text" name="search" class="form-control" id="searchForCustomer" placeholder="Search">
-		    <span class="help-block">search using first name or mobile number</span>
-		  </div>
-		</form>
+	  @include('partials._searchBar')
 
 	</div>
 
---
+++ resources/views/partials/_searchBar.blade.php
@@ -0,0 +1,6 @@
+<form role="search">
+  <div class="form-group">
+    <input type="text" name="search" class="form-control" id="searchForCustomer" placeholder="Search">
+    <span class="help-block">search using first name or mobile number</span>
+  </div>
+</form>
