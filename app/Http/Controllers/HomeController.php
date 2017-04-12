65996bda319a51105ecb0eb30068fd97dda9f357
Wale Salami 2 <wale@wsalami.com>
2017-04-12 22:56:56 +0100

    working on charts

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Charts;
 use App\Customer;
+use App\Transaction;
 
 class HomeController extends Controller
 {
@@ -25,13 +26,26 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $chart = Charts::database(Customer::all(), 'bar', 'highcharts')
-            ->elementLabel("Total")
-            ->title('Daily Customer Chart')
-            ->dimensions(0, 500)
-            ->responsive(false)
-            ->groupByDay();
+        $transaction = Transaction::all();
+        
+        $chart = Charts::create('line', 'highcharts')
+             ->title('Transactions Chart')
+             ->elementLabel('Total Naira')
+             ->labels($transaction->pluck('created_at'))
+             ->values($transaction->pluck('total_naira'), $transaction->pluck('uk_pound'))
+             ->responsive(true);
+
+            // $chart = Charts::database(Customer::all(), 'bar', 'highcharts')
+            // ->elementLabel("Total")
+            // ->title('Daily Customer Chart')
+            // ->dimensions(0, 500)
+            // ->responsive(true)
+            // ->groupByDay();
 
         return view('home', ['chart' => $chart]);
     }
 }
+
+/*
+*   bar     pie     donut   geo     gauge
+*/
