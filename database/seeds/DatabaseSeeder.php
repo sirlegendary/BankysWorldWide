4299b853780669b115390a60c72f582b708b4492
Wale Salami 2 <wale@wsalami.com>
2017-04-12 16:28:23 +0100

    Database seeder phonenumber update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
 
-        $users = factory(App\User::class, 3)->create();
+        // $users = factory(App\User::class, 3)->create();
 
         $customer = factory(App\Customer::class, 30)->create();
     }
